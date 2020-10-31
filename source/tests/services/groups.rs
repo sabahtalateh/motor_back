76ef95db66d1eab9720087ddbf9edd557caf1b6a
Aleksandr Kravtsov <sabahtalateh@gmail.com>
2020-11-01 01:26:23 +0300

    groups test

--- source/tests/services/groups.rs
+++ source/tests/services/groups.rs
@@ -53,6 +53,45 @@ async fn can_create_group_when_no_groups_created() {
 }
 
 #[actix_rt::test]
+async fn objects_empty_if_nothing_inserted() {
+    let (ctr, user): (Container, User) = drop_and_setup_with_default_user().await;
+    let groups_service: &dyn GroupsServiceIf = ctr.resolve_ref();
+
+    let response = groups_service
+        .list(
+            &user,
+            &Paging {
+                offset: 0,
+                limit: 100,
+            },
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.objects, vec![]);
+}
+
+#[actix_rt::test]
+async fn pagination_params_correct() {
+    let (ctr, user): (Container, User) = drop_and_setup_with_default_user().await;
+    let groups_service: &dyn GroupsServiceIf = ctr.resolve_ref();
+
+    let response = groups_service
+        .list(
+            &user,
+            &Paging {
+                offset: 19,
+                limit: 84,
+            },
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.offset, 19);
+    assert_eq!(response.limit, 84);
+}
+
+#[actix_rt::test]
 async fn groups_presented_after_insertion() {
     let (ctr, user): (Container, User) = drop_and_setup_with_default_user().await;
     let groups_service: &dyn GroupsServiceIf = ctr.resolve_ref();
@@ -63,39 +102,66 @@ async fn groups_presented_after_insertion() {
         .await
         .unwrap();
 
-    // let group_0 = groups_service
-    //     .list(
-    //         &user,
-    //         &Paging {
-    //             offset: 0,
-    //             limit: 1,
-    //         },
-    //     )
-    //     .await
-    //     .unwrap()
-    //     .get(0)
-    //     .unwrap()
-    //     .clone();
-    // assert_eq!(group_0, inserted_group_0);
-    //
-    // let group_1 = groups_service
-    //     .list(
-    //         &user,
-    //         &Paging {
-    //             offset: 1,
-    //             limit: 1,
-    //         },
-    //     )
-    //     .await
-    //     .unwrap()
-    //     .get(0)
-    //     .unwrap()
-    //     .clone();
-    // assert_eq!(group_1, inserted_group_1);
+    let groups = groups_service
+        .list(
+            &user,
+            &Paging {
+                offset: 0,
+                limit: 2,
+            },
+        )
+        .await
+        .unwrap()
+        .objects;
+
+    assert_eq!(groups.get(0).unwrap().clone(), inserted_group_0);
+    assert_eq!(groups.get(1).unwrap().clone(), inserted_group_1);
 }
 
-// #[actix_rt::test]
-// async fn () {
-//     let (ctr, user): (Container, User) = drop_and_setup_with_default_user().await;
-//     let groups_service: &dyn GroupsServiceIf = ctr.resolve_ref();
-// }
+#[actix_rt::test]
+async fn groups_inserted_in_correct_order() {
+    let (ctr, user): (Container, User) = drop_and_setup_with_default_user().await;
+    let groups_service: &dyn GroupsServiceIf = ctr.resolve_ref();
+
+    let inserted_group_0 = groups_service.add(&user, "group 0", None).await.unwrap();
+    let inserted_group_1 = groups_service
+        .add(&user, "group 1", Some(&inserted_group_0.id))
+        .await
+        .unwrap();
+    let inserted_group_2 = groups_service
+        .add(&user, "group 2", Some(&inserted_group_1.id))
+        .await
+        .unwrap();
+
+    let group_0 = groups_service
+        .list(
+            &user,
+            &Paging {
+                offset: 0,
+                limit: 1,
+            },
+        )
+        .await
+        .unwrap()
+        .objects
+        .get(0)
+        .unwrap()
+        .clone();
+
+    assert_eq!(group_0, inserted_group_0);
+
+    let groups_1_and_2 = groups_service
+        .list(
+            &user,
+            &Paging {
+                offset: 1,
+                limit: 10,
+            },
+        )
+        .await
+        .unwrap()
+        .objects;
+
+    assert_eq!(groups_1_and_2.get(0).unwrap().clone(), inserted_group_1);
+    assert_eq!(groups_1_and_2.get(1).unwrap().clone(), inserted_group_2);
+}
